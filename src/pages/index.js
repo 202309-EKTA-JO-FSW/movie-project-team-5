4706def4bccb1da09b38d4c9eb17551cb423decd
Gorgees Odisho <gorgees041@gmail.com>
2023-11-30 08:12:51 +0300

    fetch last movies data in home page and create Collection & Card components to show the data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,18 @@
-export default function Home() {
-  return <main className="h-[100vh]">Context</main>
+import Collection from "@/components/Collection"
+import { fetchLatestMovies } from "@/lib/data"
+
+export async function getStaticProps() {
+  const latestMovies = await fetchLatestMovies()
+  return { props: { latestMovies: latestMovies.results } }
+}
+
+export default function Home({ latestMovies }) {
+  // collection component return a collextion of card with row scrolling
+  return (
+    <main className="h-[100vh] mx-[20px] md:mx-[100px] mt-[20px]">
+      <div>
+        <Collection data={latestMovies} />
+      </div>
+    </main>
+  )
 }
