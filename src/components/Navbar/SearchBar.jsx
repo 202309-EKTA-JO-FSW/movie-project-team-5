2f4aa260d8c569691ba749d8dfc715aa3f8441c2
Gorgees Odisho <gorgees041@gmail.com>
2023-12-01 07:08:51 +0300

    create a SearchBar component #5

---
+++ src/components/Navbar/SearchBar.jsx
@@ -0,0 +1,15 @@
+const SearchBar = () => {
+  return (
+    <div>
+      <div bg-wjite>
+        <input
+          type="text"
+          placeholder="search..."
+          className="p-1 border rounded text-black"
+        />
+      </div>
+    </div>
+  )
+}
+
+export default SearchBar
