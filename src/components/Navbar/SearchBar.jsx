9ea4a47fe692289f82be44ebe75edb3f1f254d7a
Gorgees Odisho <gorgees041@gmail.com>
2023-12-02 02:47:39 +0300

    create a query params to pass it in url #5

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,4 +1,16 @@
+import { useSearchParams } from "next/navigation"
+
 const SearchBar = () => {
+  const searchParams = useSearchParams()
+
+  const handleChanges = (term) => {
+    const params = new URLSearchParams(searchParams)
+    if (term) {
+      params.set("query", term)
+    } else {
+      params.delete("query")
+    }
+  }
   return (
     <div>
       <div>
@@ -6,6 +18,7 @@ const SearchBar = () => {
           type="text"
           placeholder="search..."
           className="p-1 border rounded text-black"
+          onChange={(e) => handleChanges(e.target.value)}
         />
       </div>
     </div>
