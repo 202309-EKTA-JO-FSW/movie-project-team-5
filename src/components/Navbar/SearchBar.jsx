bca0658ca9f460cd807b603a2571ef830cad4f47
Gorgees Odisho <gorgees041@gmail.com>
2023-12-02 02:47:39 +0300

    create a query params to pass it in url #5

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
 const SearchBar = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
+=======
+import { useSearchParams } from "next/navigation"
+
+const SearchBar = () => {
+  const searchParams = useSearchParams()
+>>>>>>> 9ea4a47 (create a query params to pass it in url #5)
 
   const handleChanges = (term) => {
     const params = new URLSearchParams(searchParams)
@@ -12,11 +19,15 @@ const SearchBar = () => {
     } else {
       params.delete("query")
     }
+<<<<<<< HEAD
     const queryString = params.toString() ? `?${params.toString()}` : ""
 
     replace(`${pathname}${queryString}`)
   }
 
+=======
+  }
+>>>>>>> 9ea4a47 (create a query params to pass it in url #5)
   return (
     <div>
       <div>
@@ -26,7 +37,10 @@ const SearchBar = () => {
           placeholder="search..."
           className="p-1 border rounded text-black"
           onChange={(e) => handleChanges(e.target.value)}
+<<<<<<< HEAD
           defaultValue={searchParams.get("query")?.toString()}
+=======
+>>>>>>> 9ea4a47 (create a query params to pass it in url #5)
         />
       </div>
     </div>
