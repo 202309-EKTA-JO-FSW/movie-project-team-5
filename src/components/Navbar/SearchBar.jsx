e78793980710f8dd9b04a7ebd118d0228501b5d9
Gorgees Odisho <gorgees041@gmail.com>
2023-12-02 03:03:42 +0300

    add query in link when write in search engin

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,7 +1,9 @@
-import { useSearchParams } from "next/navigation"
+import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
 const SearchBar = () => {
   const searchParams = useSearchParams()
+  const pathname = usePathname()
+  const { replace } = useRouter()
 
   const handleChanges = (term) => {
     const params = new URLSearchParams(searchParams)
@@ -10,15 +12,21 @@ const SearchBar = () => {
     } else {
       params.delete("query")
     }
+    const queryString = params.toString() ? `?${params.toString()}` : ""
+
+    replace(`${pathname}${queryString}`)
   }
+
   return (
     <div>
       <div>
         <input
           type="text"
+          name="search"
           placeholder="search..."
           className="p-1 border rounded text-black"
           onChange={(e) => handleChanges(e.target.value)}
+          defaultValue={searchParams.get("query")?.toString()}
         />
       </div>
     </div>
