6b83a064e09068c96851c542cc2a5f420d7dfea6
Gorgees Odisho <144837653+gorgees04@users.noreply.github.com>
2023-12-01 20:37:59 -0800

    Update SearchBar.jsx

--- src/components/Navbar/SearchBar.jsx
+++ src/components/Navbar/SearchBar.jsx
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
+
 "use client"
 import { fetchSearchMovies } from "@/lib/data"
->>>>>>> f4cde26 (fetching search data from searchParams)
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
 import SearchDropDown from "./SearchDropDown"
@@ -14,20 +10,19 @@ const SearchBar = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
-=======
+
 import { useSearchParams } from "next/navigation"
 
 const SearchBar = () => {
   const searchParams = useSearchParams()
->>>>>>> 9ea4a47 (create a query params to pass it in url #5)
-=======
+
 import { usePathname, useRouter, useSearchParams } from "next/navigation"
 
 const SearchBar = () => {
   const searchParams = useSearchParams()
   const pathname = usePathname()
   const { replace } = useRouter()
->>>>>>> 2f9755c (add query in link when write in search engin #5)
+
 
   const handleChanges = useDebouncedCallback((term) => {
     const params = new URLSearchParams(searchParams)
@@ -36,21 +31,13 @@ const SearchBar = () => {
     } else {
       params.delete("query")
     }
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 2f9755c (add query in link when write in search engin #5)
+
     const queryString = params.toString() ? `?${params.toString()}` : ""
 
     replace(`${pathname}${queryString}`)
   }, 300)
 
-<<<<<<< HEAD
-=======
-  }
->>>>>>> 9ea4a47 (create a query params to pass it in url #5)
-=======
->>>>>>> 2f9755c (add query in link when write in search engin #5)
+
   return (
     <div>
       <div className="relative group w-[270px]">
@@ -60,14 +47,7 @@ const SearchBar = () => {
           placeholder="search..."
           className="p-1 border rounded text-black"
           onChange={(e) => handleChanges(e.target.value)}
-<<<<<<< HEAD
-<<<<<<< HEAD
           defaultValue={searchParams.get("query")?.toString()}
-=======
->>>>>>> 9ea4a47 (create a query params to pass it in url #5)
-=======
-          defaultValue={searchParams.get("query")?.toString()}
->>>>>>> 2f9755c (add query in link when write in search engin #5)
         />
         <div className="absolute top-[100%] transition duration-300 bg-white text-black z-50 mt-1 rounded overflow-y-auto">
           <SearchDropDown />
