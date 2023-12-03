371e7cc13bb475a1c11ff9a2a510758e8f723356
Gorgees Odisho <gorgees041@gmail.com>
2023-12-03 06:58:05 +0300

    improving dropdown navbar box #25

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -17,19 +17,22 @@ const NavBar = () => {
   const movies = ["Latest", "Popular", "Top_Rated", "Upcoming"]
   return (
     <nav className="flex flex-col items-center justify-center h-[100px] bg-black text-white sm:flex-row">
-      <div className="flex items-center w-full p-5 font-bold sm:ml-5">
-        <div className="md:text-lg">
+      <div className="flex items-center w-full p-5 sm:ml-5">
+        <div className="md:text-lg text-cyan-600 font-bold">
           <Link href={"/"}>Logo</Link>
         </div>
         <div className="flex w-full justify-center sm:justify-around sm:w-[300px] sm:ml-10 ">
           <div className="relative group mr-4">
-            <button className="focus:outline-none md:text-lg text-cyan-600">
+            <button className="focus:outline-none md:text-lg text-cyan-600 font-bold">
               Genres
             </button>
-            <div className="absolute top-full left-0 bg-white py-2 px-1 hidden group-hover:flex flex-col text-black z-50 text-sm">
+            <div className="absolute top-full left-0 bg-slate-100 shadow-lg shadow-black py-2 px-3 rounded hidden group-hover:flex flex-col text-black z-50 text-sm h-[200px] overflow-y-auto">
               {genresList.map((genres) => {
                 return (
-                  <div className=" hover:bg-blue-400" key={genres.id}>
+                  <div
+                    className="hover:bg-cyan-600 p-1 rounded"
+                    key={genres.id}
+                  >
                     <Link href={`/movies/${genres.name.toLocaleLowerCase()}`}>
                       {genres.name}
                     </Link>
@@ -39,13 +42,13 @@ const NavBar = () => {
             </div>
           </div>
           <div className="relative group mr-4">
-            <button className="focus:outline-none md:text-lg text-cyan-600">
+            <button className="focus:outline-none md:text-lg text-cyan-600 font-bold">
               Movies
             </button>
-            <div className="absolute top-full left-0 bg-white py-2 px-1 hidden group-hover:flex flex-col text-black z-50">
+            <div className="absolute top-full left-0 bg-slate-100 shadow-lg shadow-black py-2 px-3 rounded hidden group-hover:flex flex-col text-black z-50 text-sm">
               {movies.map((genres, id) => {
                 return (
-                  <div className=" hover:bg-blue-400" key={id}>
+                  <div className="hover:bg-cyan-600 p-1 rounded" key={id}>
                     <Link
                       href={
                         genres === "Latest"
@@ -60,7 +63,7 @@ const NavBar = () => {
               })}
             </div>
           </div>
-          <div className="focus:outline-none md:text-lg text-cyan-600">
+          <div className="focus:outline-none md:text-lg text-cyan-600 font-bold">
             <Link href={"/actors"}>Actors</Link>
           </div>
         </div>
