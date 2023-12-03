13f240769709bbd0a3420f02c42f25d18b1509b8
Gorgees Odisho <gorgees041@gmail.com>
2023-12-04 01:13:21 +0300

    rendering top movies rated in home page #17

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,27 @@
 import LatestMovies from "@/components/home/LatestMovies"
 import PopularMovies from "@/components/home/PopularMovies"
-import { fetchLatestMovies, fetchPopularMovies } from "@/lib/data"
+import TopRatedMovies from "@/components/home/TopRatedMovies"
+import {
+  fetchLatestMovies,
+  fetchPopularMovies,
+  fetchTopRatedMovies,
+} from "@/lib/data"
 
 export async function getStaticProps() {
   const latestMovies = await fetchLatestMovies()
   const popularMovies = await fetchPopularMovies()
+  const topRatedMovies = await fetchTopRatedMovies()
 
   return {
     props: {
       latestMovies: latestMovies.results,
       popularMovies: popularMovies.results,
+      topRatedMovies: topRatedMovies.results,
     },
   }
 }
 
-export default function Home({ latestMovies, popularMovies }) {
+export default function Home({ latestMovies, popularMovies, topRatedMovie }) {
   // collection component return a collextion of card with row scrolling
   if (!latestMovies) {
     return <h1 className="text-xl">Loading.....</h1>
@@ -23,6 +30,7 @@ export default function Home({ latestMovies, popularMovies }) {
     <main className="h-[100vh] mx-[20px] md:mx-[100px] mt-[20px]">
       <LatestMovies data={latestMovies} />
       <PopularMovies data={popularMovies} />
+      <TopRatedMovies data={topRatedMovie} />
     </main>
   )
 }
