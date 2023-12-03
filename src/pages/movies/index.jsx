957538581e2085894be37436a6e55f5734f66001
banan <banan.faouri2001@gmail.com>
2023-12-03 04:40:32 +0300

    fetching latest movies in movies page #22

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -1,7 +1,26 @@
 import React from "react"
+import LatestMovies from "@/components/home/LatestMovies"
+import { fetchLatestMovies } from "@/lib/data"
 
-const Movies = () => {
-  return <div>banan is start working in movies page</div>
+export async function getStaticProps() {
+  const latestMovies = await fetchLatestMovies()
+  
+
+  return {
+    props: {
+      latestMovies: latestMovies.results
+    },
+  }
 }
 
-export default Movies
+const Movies = ({latestMovies}) => {
+  if (!latestMovies) {
+    return <h1 className="text-xl">Loading.....</h1>
+  }
+  return (
+    <main className="h-[100vh] mx-[20px] md:mx-[100px] mt-[20px]">
+      <LatestMovies data={latestMovies} />
+    </main>
+  )
+  }
+export default Movies;
