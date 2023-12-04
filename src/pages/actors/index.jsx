5caa308119b5757ac0872971772df49533d1c065
Hasan <h.mjabai92@gmail.com>
2023-12-04 13:25:50 +0300

    actors page

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,7 +1,39 @@
-import React from "react"
+import React from "react";
+import Link from "next/link";
 
-const Actors = () => {
-  return <div>Actors</div>
+export default async function page() {
+  // const res = await fetch("/actors");
+  /// need to fetch data//////
+  const actor = await res.json();
+  const actors = actor.map((e) => {
+    return (
+      <Link href={`/actors/${e.id}`}>
+        <div
+          style={{
+            width: "100%",
+            padding: "10px",
+            background: "white",
+            color: "black",
+            borderRadius: "10px",
+            marginTop: "20px",
+          }}
+        >
+          <h1>{e.title}</h1>
+          <p>{e.body}</p>
+        </div>
+      </Link>
+    );
+  });
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      {actors}
+    </div>
+  );
 }
-
-export default Actors
