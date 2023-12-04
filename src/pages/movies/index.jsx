4215b20bdbab52d13d577c882b21044240a4b912
banan <banan.faouri2001@gmail.com>
2023-12-05 01:13:35 +0300

    latest movies with pagination done

--- src/pages/movies/index.jsx
+++ src/pages/movies/index.jsx
@@ -1,7 +1,55 @@
 import React from "react"
+import Card from "@/components/Card"
+import { fetchLatestMovies } from "@/lib/data"
+import { useState } from "react"
 
-const Movies = () => {
-  return <div>banan is start working in movies page</div>
+
+export async function getStaticProps() {
+  const initialMovies = await fetchLatestMovies(1);
+  return { props: { initialMovies: initialMovies.results } };
 }
 
-export default Movies
+const Movies = ({ initialMovies }) => {
+  const [movies, setMovies] = useState(initialMovies);
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const fetchMoreMovies = async () => {
+    const nextPage = currentPage + 1;
+    const newMovies = await fetchLatestMovies(nextPage);
+    setMovies([...newMovies.results])
+    setCurrentPage(nextPage);
+  };
+
+  const fetchPreviousMovies = async () => {
+    if (currentPage > 1) {
+      const prevPage = currentPage - 1;
+      const newMovies = await fetchLatestMovies(prevPage);
+      setMovies([...newMovies.results]);
+      setCurrentPage(prevPage);
+    }
+  };
+
+  return (
+    <div>
+      <h1 className="focus:outline-none ">Latest Movies</h1>
+      <div className="flex flex-wrap">
+        {movies.map((movie) => (
+          <Card key={movie.id} category={movie} />
+        ))}
+      </div>
+      
+      <button
+          onClick={fetchPreviousMovies}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+        >
+          Previous
+        </button>
+      <button onClick={fetchMoreMovies}
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4"
+      >Next Page</button>
+    </div>
+    
+  );
+};
+
+export default Movies;
