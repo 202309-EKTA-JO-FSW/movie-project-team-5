3bb9821e2ffd9c98f2adb7a716402fb1b465f0fb
hakeema51.alsoudi@gmail.com <hakeema51.alsoudi@gmail.com>
2023-12-05 18:53:43 +0300

    working in movie information component

--- src/components/movies/MovieInfo.jsx
+++ src/components/movies/MovieInfo.jsx
@@ -18,13 +18,43 @@ const MovieInfo = ({ movie }) => {
     genres,
     spoken_languages,
   }
+  // check the image url ...........
+  const imgUrl = `https://${backdrop_path}`
 
   return (
     <div>
-      <p>{movie.id}</p>
-      <p>{movie.poster}</p>
-      <h1>{movie.overview}</h1>
-      <p>{movie.description}</p>
+      <img src={imgUrl} alt={title} />
+      <h1>{title}</h1>
+      <p>Release Date: {release_date}</p>
+      <p>Runtime: {runtime} minutes</p>
+      <p>Language: {original_language}</p>
+      <p>
+        Rating: {vote_average} ({vote_count} votes)
+      </p>
+      <p>Overview: {overview}</p>
+      <h2>Genres</h2>
+      <ul>
+        {genres.map((genre) => (
+          <li key={genre.id}>{genre.name}</li>
+        ))}
+      </ul>
+      <h2>Spoken Languages</h2>
+      <ul>
+        {/* here i choose the english language as general language */}
+        {spoken_languages.map((language) => (
+          <li key={language.iso_639_1}>{language.name}</li>
+        ))}
+      </ul>
+      <h2>Production Companies</h2>
+      {production_companies.map((company) => (
+        <div key={company.id}>
+          <img
+            src={`https://${company.logo_path}`} // check the source here
+            alt={company.name}
+          />
+          <p>{company.name}</p>
+        </div>
+      ))}
     </div>
   )
 }
