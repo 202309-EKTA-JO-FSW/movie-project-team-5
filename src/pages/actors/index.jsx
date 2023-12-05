1fd7ba4bd1f115d2e4a333ec48d6df6784fe85ab
Hasan <h.mjabai92@gmail.com>
2023-12-05 20:24:15 +0300

    actor page

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,39 +1,41 @@
-import React from "react";
-import Link from "next/link";
+import { useEffect, useState } from "react"
+import axios from "axios"
+import { getImage } from "@/lib/utils"
+import ActorCard from "./ActorCard"
+export default function ActorList() {
+  const [actors, setActors] = useState([])
 
-export default async function page() {
-  // const res = await fetch("/actors");
-  /// need to fetch data//////
-  const actor = await res.json();
-  const actors = actor.map((e) => {
-    return (
-      <Link href={`/actors/${e.id}`}>
-        <div
-          style={{
-            width: "100%",
-            padding: "10px",
-            background: "white",
-            color: "black",
-            borderRadius: "10px",
-            marginTop: "20px",
-          }}
-        >
-          <h1>{e.title}</h1>
-          <p>{e.body}</p>
-        </div>
-      </Link>
-    );
-  });
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.themoviedb.org/3/person/popular?api_key=da3c06c669cc47bf0856ca78b85cfe7c",
+      )
+      .then((response) => {
+        console.log(response.data.results.id)
+        setActors(response.data.results)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [])
+
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      {actors}
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      {actors.map((actor, index) => {
+        return (
+          <div>
+            {/* <img
+              src={getImage(actor.profile_path)}
+              alt=""
+              style={{ width: "200px" }}
+            />
+            <h1>{actor.name}</h1> */}
+
+            {/* <p>{actor.profile_path}</p> */}
+            <ActorCard actor={actor} />
+          </div>
+        )
+      })}
     </div>
-  );
+  )
 }
