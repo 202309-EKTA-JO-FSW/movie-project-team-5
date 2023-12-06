88a80c35548cb578483dd0af20e6dd5b55d4acd0
Gorgees Odisho <gorgees041@gmail.com>
2023-12-06 23:44:45 +0300

    adding the category reducer to the store #34

--- src/app/store.js
+++ src/app/store.js
@@ -1,5 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit"
+import categoryReducer from "@/feature/category/categorySlice"
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    // if curious from where the categoryReducer came from
+    // name like this just to explain that we geting the reducer but in actual is returning categorySlice.reducer
+    category: categoryReducer,
+  },
 })
