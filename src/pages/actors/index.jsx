5e7933c5c4f20362e7df3bf7e64d85f6f4e1a918
Hasan <h.mjabai92@gmail.com>
2023-12-07 01:27:23 +0300

    creat actor page

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,41 +1,24 @@
-import { useEffect, useState } from "react"
-import axios from "axios"
-import { getImage } from "@/lib/utils"
-import ActorCard from "./ActorCard"
-export default function ActorList() {
-  const [actors, setActors] = useState([])
 
-  useEffect(() => {
-    axios
-      .get(
-        "https://api.themoviedb.org/3/person/popular?api_key=da3c06c669cc47bf0856ca78b85cfe7c",
-      )
-      .then((response) => {
-        console.log(response.data.results.id)
-        setActors(response.data.results)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }, [])
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/person/popular?api_key=da3c06c669cc47bf0856ca78b85cfe7c",
+  )
+  const data = await res.json()
+  return {
+    props: { Actors: data },
+  }
+}
 
+const actors = ({ Actors }) => {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      {actors.map((actor, index) => {
-        return (
-          <div>
-            {/* <img
-              src={getImage(actor.profile_path)}
-              alt=""
-              style={{ width: "200px" }}
-            />
-            <h1>{actor.name}</h1> */}
-
-            {/* <p>{actor.profile_path}</p> */}
-            <ActorCard actor={actor} />
-          </div>
-        )
-      })}
+    <div>
+      <h1>Actor list </h1>
+      {Actors.map((Actor) => (
+        <div key={Actor.id}>
+          <h1>{Actor.name}</h1>
+        </div>
+      ))}
     </div>
   )
 }
+export default actors;
